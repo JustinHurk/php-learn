c038f123d3d37cce925f93ad71dd3956c5b1de11
Justin Van Den Hurk <justin.vandenhurk@vrdistribution.com.au>
2022-07-15 14:30:17 +0930

    pdo

--- arrays/index.php
+++ arrays/index.php
@@ -1,9 +1,57 @@
 <?php
 
-$animals = ['John', 'Jamie', 'Joe', 'Brodie', 'Drew'];
+class Post
+{
+    public $title;
 
-/* foreach ($names as $name) {
-    echo $name . ', ';
-} */
+    public $author;
 
-require 'index.view.php';
+    public $published;
+
+    public function __construct($title, $author, $published)
+    {
+        $this->title = $title;
+        $this->author = $author;
+        $this->published = $published;
+    }
+}
+
+$posts = [
+    new Post('My first Post', 'JV', true),
+    new Post('My Second Post', 'JV',true),
+    new Post('My Third Post', 'BE',true),
+    new Post('My Fourth Post', 'JS',false),
+];
+
+
+//array filter
+// $unpunlished = array_filter($posts, function ($post){
+//     return ! $post->published;
+
+// });
+
+// $publishedPosts = array_filter($posts, function ($post){
+//     return $post->published;
+
+// });
+
+// foreach ($posts as $post) {
+//     $post->published = true;
+// }
+
+//array map
+// $modified = array_map(function ($post) {
+//     return ['title' => $post->title];
+// }, $posts);
+
+// $titles = array_map(function ($post) {
+//     return $post->title;
+// }, $posts);
+
+$posts = array_map(function ($post) {
+    return (array) $post;
+}, $posts);
+
+$titles = array_column($posts, 'author');
+
+var_dump($titles);
